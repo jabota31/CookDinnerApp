9b85c9ab8cc72550ead87f05794ce4c73ff531c9
Gurja69 <leonardogmf@gmail.com>
2019-07-28 16:25:50 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { createStackNavigator } from 'react-navigation';
+
+const App = createStackNavigator({
+	Home: { screen: HomeScreen },
+	Profile: { screen: ProfileScreen },
+});
+
+export default App;
